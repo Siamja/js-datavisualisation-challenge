59abc1bf7d054ff750a8ca755c6a77e745587de9
Jérémy <jeremy.creola@gmail.com>
2018-11-16 14:21:03 +0100

    ajout tableau + div JS

--- assets/becode-app.js
+++ assets/becode-app.js
@@ -11,3 +11,27 @@ What does this script do ?
 */
 
 // Your scripting goes here...
+
+
+// creation div + graphique
+
+let currentTable = document.getElementsByTagName('table')[0];
+
+let newDiv = document.createElement("div");
+newDiv.id = "graphique1";
+
+currentTable.parentNode.insertBefore(newDiv, currentTable);
+
+//code Dimple du graphique
+
+var svg = dimple.newSvg("#graphique1", 500, 600,);
+
+var data = [
+{ "Word":"Hello", "Awesomeness":2000 },
+{ "Word":"World", "Awesomeness":3000 }
+];
+var chart = new dimple.chart(svg, data);
+chart.addCategoryAxis("x", "Word");
+chart.addMeasureAxis("y", "Awesomeness");
+chart.addSeries(null, dimple.plot.bar);
+chart.draw();
