e32d9a8e64acfbeea881afd7e2c1f0868f9b0f0a
Jérémy <jeremy.creola@gmail.com>
2018-11-20 12:02:03 +0100

    maj 1

--- assets/becode-app.js
+++ assets/becode-app.js
@@ -35,3 +35,64 @@ chart.addCategoryAxis("x", "Word");
 chart.addMeasureAxis("y", "Awesomeness");
 chart.addSeries(null, dimple.plot.bar);
 chart.draw();
+
+let arrayCountry = [];
+let arrayStats = [];
+
+// Récupérer les pays
+
+let arrayPushCountry = () => { 
+  for (i = 0; i < 420; i++) {
+    if (isNaN(parseInt(d3.select("#table1").selectAll("tbody").selectAll("td")._groups[0][i].innerText)) && d3.select("#table1").selectAll("tbody").selectAll("td")._groups[0][i].innerText != ":") {
+      arrayCountry.push((d3.select("#table1").selectAll("tbody").selectAll("td")._groups[0][i].innerText));
+    }
+  }
+
+}
+
+// Récupérer les données
+
+let arrayPushStats = () => { 
+  for (i = 0; i < 420; i++) {
+
+    if (isNaN(parseFloat(d3.select("#table1").selectAll("tbody").selectAll("td")._groups[0][i].innerText)) == false) {
+      arrayStats.push(d3.select("#table1").selectAll("tbody").selectAll("td")._groups[0][i].innerText);
+
+    }
+  }
+}
+
+let arrayStatsByCountry = [];
+
+let statsByCountry = {
+  Pays: "",
+  a2002: "",
+  b2003: "",
+  c2004: "",
+  d2005: "",
+  e2006: "",
+  f2007: "",
+  g2008: "",
+  h2009: "",
+  i2010: "",
+  j2011: "",
+  k2012: "",
+}
+let testObject = () => {
+  for (i = 0; i < arrayCountry.length; i++) {
+    let testNewObj = Object.create(statsByCountry);
+    testNewObj.Pays = arrayCountry[i];
+    for (y = 0; y < arrayStats.length; y++){
+    testNewObj.a2002 = arrayStats[i];
+    }
+    arrayStatsByCountry.push(testNewObj);
+  }
+}
+
+arrayPushCountry();
+arrayPushStats();
+testObject();
+console.log(arrayCountry);
+console.log(arrayStats);
+console.log(arrayCountry[0]);
+console.log(arrayStatsByCountry);
